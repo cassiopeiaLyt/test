413f162f2e8dbecf6038141be33e6176ebe2262a
Liuyutian <499396551@qq.com>
2017-05-19 11:38:18 +0800

    learn javascrip object

--- main/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = function main(str) {
-  var charr = str.split("");
-  var sum = 0;
-  for (var i=0; i < charr.length; i++) {
-	var temp = parseInt(charr[i]);
-	sum += temp;
-  }
-  return sum;
-};
---
+++ main/person.js
@@ -0,0 +1,12 @@
+// Write your code here
+module.exports = Person
+
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+Person.prototype.introduce = function() {
+	var str = "My name is "+this.name+". I am "+this.age+" years old.";
+	return str;
+};
