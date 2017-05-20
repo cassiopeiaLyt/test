2c6ef7a01fda72d6f6eb26427deb6efdfc74a0b3
Liuyutian <499396551@qq.com>
2017-05-20 10:18:57 +0800

    answer for question6

--- main/main.js
+++ main/main.js
@@ -1,44 +1,54 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-o) 最大值 = ${sequence.maxnum()}
-o) 元素数量 = ${sequence.size()}
-o) 平均值 = ${sequence.average()}`);
+const loadAllItems = require('./loadAllItems.js');
+
+module.exports = function main(inputs) {
+    console.log("Debug Info");
+	var item;
+	var item_list = loadAllItems();
+	var list = new Array();
+	var sum = 0;
+	//处理输入数据
+	for (var i in inputs) {
+		item.barcode = inputs[i];
+		var index = find(item, list);
+		if(index == -1) {//如果不存在
+			item.count = 1;
+			//找到商品的额外信息
+			for (var j in item_list) {
+				if(item.barcode == item_list[j].barcode) {
+					item.name = item_list[j].name;
+					item.unit = item_list[j].unit;
+					item.price = item_list[j].price;
+				}
+			}
+			list.push(item);
+		} else {
+			list[index].count ++;
+		}
+	}
+	
+	//生成输出数据
+	var title = '***<没钱赚商店>购物清单***\n';
+	var middle;
+	for (var i in list) {
+		var total = list[i].count * list[i].unit;
+		middle += '名称：'+list[i].name+'，数量：'+list[i].count+list[i].unit+'，单价：'+list[i].price+'(元)，小计：'+total+'(元)\n';
+		sum += total;
+	}
+	var tail = '----------------------\n' +
+            '总计：'+sum+'(元)\n' +
+            '**********************';
+	var info = title + middle + tail;
+	return info;
 };
 
-class Sequence {
-  constructor(input) {
-    this.input = input;
-  }
-
-  minimum() {
-    var min = 0;
-	for (var i=0; i< this.input.length; i++) {
-		if(this.input[i] < min)
-			min = this.input[i];
+module.exports = function isExist(item, list) {
+	var index = -1;
+	for(var i in list) {
+		if(list[i].barcode == item.barcode) {
+			index = i;
+			break;
+		}
 	}
-	return min;
-  }
-
-  maxnum() {
-	var max = 0;
-	for (var i=0; i< this.input.length; i++) {
-		if(this.input[i] > max)
-			max = this.input[i];
-	}
-	return max;
-  }
-  
-  size() {
-	return this.input.length;  
-  }
-  
-  average() {
-	var sum = 0;
-	for (var i=0; i< this.input.length; i++) {
-		sum += this.input[i];
-	}
-	var avg = parseFloat(sum / this.input.length).toFixed(2);
-	return avg;
-  }
+	return index;
 }
+
